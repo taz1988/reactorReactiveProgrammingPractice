e4a3a1b6588c714d22823d42ec29f792f3080690
Zoltán Kornél Török <taz19880922@gmail.com>
2019-08-08 22:04:35 +0200

    Add basic skeleton project

---
+++ src/main/java/com/practice/reactor/ReactorPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorPracticeApplication.class, args);
+	}
+
+}
